077ec774d77d04e0bc0d28ca16342d1883322b92
Tyson <02tyson.watson@gulllakecs.org>
2020-01-07 11:34:45 -0500

    Made Display and added start of java code

--- app/src/main/java/com/example/justjavasdas/MainActivity.java
+++ app/src/main/java/com/example/justjavasdas/MainActivity.java
@@ -3,12 +3,42 @@ package com.example.justjavasdas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
+/**
+
+ This app displays an order form to order coffee
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+
+     This method is called when the order is clicked.
+     */
+    public void submitOrder(View view) {
+        display((77*2)+1);
+    }
+    /**
+
+     This method displays the given quantity value on screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance(Locale.US).format(number));
     }
 }
